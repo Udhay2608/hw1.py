7213bf95d0aa84c8960a762024ed68bc823fa14a
Udhay2608 <U-Tiwana23@st-bernards.slough.sch.uk>
2026-04-23 19:03:24 +0100

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,20 @@
+class RomanConverter:
+    def int_to_roman(self, num):
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+        roman = ""
+        i = 0
+
+        while num > 0:
+            while num >= values[i]:
+                roman += symbols[i]
+                num -= values[i]
+            i += 1
+
+        return roman
+
+
+n = int(input("Enter a number: "))
+converter = RomanConverter()
+print(converter.int_to_roman(n))
